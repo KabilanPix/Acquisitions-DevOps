acfbb910442b7084c0ab5c38b5a9840070416b49
Kabilan <kabilan629@gmail.com>
2026-06-03 15:26:35 +0530

    Implement Auth & Cookies

---
+++ src/utils/jwt.js
@@ -0,0 +1,28 @@
+import logger from '#config/logger.js';
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || 'secret-key';
+
+const JWT_EXPIRES_IN = '1d';
+
+export const jwttoken = {
+  sign : (payload) => {
+    try{
+      return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+    }
+    catch(e){
+      logger.error('Failed to authenticate token', e);
+      throw new Error('Failed to authentiacte token',{ cause: e });
+    }
+  },
+  verify : (token) => {
+    try{
+      return jwt.verify(token, JWT_SECRET);
+
+    }
+    catch(e){
+      logger.error('Failed to authenticate token', e);
+      throw new Error('Failed to authentiacte token',{ cause: e });
+    }
+  }
+};
